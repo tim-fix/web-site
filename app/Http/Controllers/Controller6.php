d9a5bc6a73e6d7bdd86117c7ec309f1bc3fd2550
tim-fix <timur2007122@gmail.com>
2026-02-27 01:33:07 -0800

    last6Commit

--- app/Http/Controllers/Controller6.php
+++ app/Http/Controllers/Controller6.php
@@ -7,7 +7,14 @@ class Controller6 extends Controller
     public function show()
 
         {		
-		    $collection = collect([1, 2, 3]);
-            return $collection;
+		    $collection = collect([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]);
+
+$collapsed = $collection->collapse();
+
+return $collapsed->all();
 		}
 }
