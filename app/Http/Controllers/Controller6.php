9ac0ef0724b246d89e1b527d9e476723465434fb
tim-fix <timur2007122@gmail.com>
2026-03-02 19:34:11 -0800

    commit6-lab6

--- app/Http/Controllers/Controller6.php
+++ app/Http/Controllers/Controller6.php
@@ -6,9 +6,57 @@ class Controller6 extends Controller
 {
     public function show()
 
-        {		
-		    $collection = collect([1, 2, 3, 4]);
+    {
+        $collection = collect([
+    [
+        'name' => 'John Doe',
+        'department' => 'Sales',
+    ],
+    [
+        'name' => 'Jane Doe',
+        'department' => 'Sales',
+    ],
+    [
+        'name' => 'Johnny Doe',
+        'department' => 'Marketing',
+    ]
+]);
 
-return $collection->count();
-		}
+$grouped = $collection->mapToGroups(function ($item, $key) {
+    return [$item['department'] => $item['name']];
+});
+
+$grouped->all();
+
+/*
+    [
+        'Sales' => ['John Doe', 'Jane Doe'],
+        'Marketing' => ['Johnny Doe'],
+    ]
+*/
+
+$grouped->get('Sales')->all();
+dd($grouped);
+    }
+    
 }
+class Currency
+{
+    /**
+     * Создать новый экземпляр валюты.
+     *
+     * @param  string  $code
+     * @return void
+     */
+    function __construct(string $code)
+    {
+        $this->$code = $code;
+    }
+}
+
+$collection = collect(['USD', 'EUR', 'GBP']);
+
+$currencies = $collection->mapInto(Currency::class);
+
+$currencies->all();
+
