527f012ffc61f9cf3fb89ffeab9bef347eedaa40
tim-fix <timur2007122@gmail.com>
2026-03-06 23:25:15 -0800

    PhP

--- app/Http/Controllers/Controller1.php
+++ app/Http/Controllers/Controller1.php
@@ -3,8 +3,23 @@ namespace App\Http\Controllers;
 
 class Controller1 extends Controller
 {
+    public function hello(){
+        return view('post.hello', [
+            'title' => 'page title',
+            'text' => 'page content',
+            ]);
+    }
+
     public function show(){
-        echo "Hello";
+        return 'text - "Hello"';
+    }
+
+    public function kaif(){
+        return view('post.zadanie',[
+            'title' => 'google.com',
+            'name'=> 'Николай',
+            'surname' => 'Фёдорович'
+        ]);
     }
 }
 ?>
