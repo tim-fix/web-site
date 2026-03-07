d02dfa48fc26547637b94b542a6fb6b7ec2bacfb
tim-fix <timur2007122@gmail.com>
2026-03-06 23:09:34 -0800

    lab7

---
+++ database/migrations/2026_03_07_063652_change_posts_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangePostsTable extends Migration
+	{
+		public function up()
+		{
+		Schema::table('posts', function (Blueprint $table) {
+		$table->renameColumn('name', 'title');
+	});
+
+
+
+
+		}
+	
+		public function down()
+		{
+			Schema::dropIfExists('posts');
+		}
+	}
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Controller6;
 
+use App\Http\Controllers\Controller7;
+
 Route::get('/show', [Controller6::class,'show']);
