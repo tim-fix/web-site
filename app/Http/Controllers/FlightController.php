a7a6c58a47ba3c914e526a6b0fc3ed314baa6590
tim-fix <timur2007122@gmail.com>
2026-03-18 20:35:00 -0700

    lab10-1

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -10,11 +10,10 @@ class FlightController extends Controller
      public function show()
     {
         $flights = Flight::all();
-        dd($flights);
-        return view('flight.index', ['flights' => $flights]);
-
         foreach ($flights as $flight) {
   echo $flight->name;
-}
+        }
+        dd($flights);
+        
     }
 }
--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Flight extends Model
 {
-    //
+    protected $fillable = ['name'];
 }
