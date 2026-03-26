605c8e61f45f479d9f06fe09a77c84cbfd8c4beb
tim-fix <timur2007122@gmail.com>
2026-03-25 20:30:09 -0700

    lab11

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Posts;
 
 class PostsController extends Controller
 {
-    //
+    public function getAll($order = 'created_at') 
+    {
+        $posts = Posts::orderBy($order)->get();
+
+        dd($posts);
+    }
+    public function getOne($id)
+    {
+        $posts = Posts::orderBy('created_at')->get();
+        dd($posts);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\FlightController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/show', [FlightController::class,'show']);
-Route::get('/phone', [UserController::class,'phone']);
+Route::get('/post/all/{order?}', [PostsController::class,'getAll']);
+Route::get('/post/{id}', [PostsController::class,'getOne']);
